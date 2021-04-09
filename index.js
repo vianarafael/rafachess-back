6f3ca268016018904ef0ce62145554b21f951fbc
vianarafael <vianarafaelds@gmail.com>
2021-04-09 18:47:06 +0900

    server finished

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const socket = require("socket.io");
+const cors = require("cors");
+
+// app setup
+const port = 1234;
+const server = app.listen(port, () =>
+  console.log(`listening to request on port ${port}`)
+);
+
+
+app.use(cors())
+// app.use(express.static("public"));
+
+// socket setup
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("made socket connection");
+  socket.on("move", (data) => {
+    io.emit("move", data);
+  });
+});
