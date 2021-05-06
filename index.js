83d60a21c692b5f9e9de95b588d7672f5a3a327a
vianarafael <vianarafaelds@gmail.com>
2021-05-06 10:25:51 +0900

    hacky solution to avoid players moving pieces outside of their turn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const server = app.listen(port, () =>
 
 
 app.use(cors())
-// app.use(express.static("public"));
 
 // socket setup
 const io = socket(server, {
@@ -27,8 +26,14 @@ const io = socket(server, {
   },
 });
 
+let numClients = 0;
+
 io.on("connection", (socket) => {
   console.log("made socket connection");
+  numClients++
+  console.log(numClients)
+  if (numClients > 2) numClients = 1
+  io.emit('stats', numClients)
   socket.on("move", (data) => {
     io.emit("move", data);
   });
